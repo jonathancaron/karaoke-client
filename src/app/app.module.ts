6c12568b2832fbe140364233d4b068fc2f1447b6
jonathancaron <jonathan.caron04@gmail.com>
2018-02-13 16:32:00 +0100

    Structure client début : slides informatifs et début système connexion/enregistrement

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { MusicPage } from '../pages/music/music';
+import { NewsPage } from '../pages/news/news';
+import { VideoPage } from '../pages/video/video';
+import { TabsPage } from '../pages/tabs/tabs';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MusicPage,
+    NewsPage,
+    VideoPage,
+    TabsPage,
+    WelcomePage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MusicPage,
+    NewsPage,
+    VideoPage,
+    TabsPage,
+    WelcomePage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
